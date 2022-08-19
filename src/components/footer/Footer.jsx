b9d834e2f6fd69a27b39176723be83c5bbe069e7
MANIKANT MANI <manikantmani12345@gmail.com>
2022-08-19 16:19:21 +0530

    contact_me_add

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,14 +4,13 @@ import "./footer.css";
 const Footer = () => {
   return (
     <>
-        <div className="social_media" style={{width:"100% !important"}}>
-               {/* <span className="social_media_heading">Follow me on social site...</span> */}
-                   <span className="links"><a className='aLink' href="https://github.com/ManikantMani" target="_blank"><i class="fab fa-github"></i></a></span>
-                   {/* <span className="links"><a className='aLink' href="manikantmani12345@gmail.com" target="_blank"><i class="fas fa-envelope-square"></i></a></span> */}
-                   <span className="links"><a className='aLink' href="https://www.linkedin.com/in/manikant-mani-4a7606216" target="_blank"><i class="fab fa-linkedin"></i></a></span>
-                   <span className="links"><a className='aLink' href="https://www.youtube.com/channel/UC4so0nZsVoydtosEXqq8eFg/featured" target="_blank"><i class="fab fa-youtube"></i></a></span>
-                   <span className="links"><a className='aLink' href="https://www.instagram.com/?hl=en"><i class="fab fa-instagram" target="_blank"></i></a></span>
-               </div>
+      <div className="social_media" style={{ width: "100% !important" }}>
+      <span className="links"><a className='aLink' href="tel:7004336713" target="_blank"><i class="fa-solid fa-phone"></i></a></span>
+        <span className="links"><a className='aLink' href="https://wa.me/917004336713" target="_blank"><i class="fa-brands fa-whatsapp"></i></a></span>
+        <span className="links"><a className='aLink' href="mailto:manikantmani12345@gmail.com" target="_blank"><i class="fa-solid fa-envelope"></i></a></span>
+        <span className="links"><a className='aLink' href="https://www.linkedin.com/in/manikant-mani-4a7606216" target="_blank"><i class="fab fa-linkedin"></i></a></span>
+        <span className="links"><a className='aLink' href="https://github.com/ManikantMani" target="_blank"><i class="fab fa-github"></i></a></span>
+      </div>
     </>
   )
 }
