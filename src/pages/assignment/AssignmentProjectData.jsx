86cec381d2a32788faf1590d5d6aec220f0a02ac
MANIKANT MANI <manikantmani12345@gmail.com>
2022-08-19 16:56:01 +0530

    addingNewAssignment_project

--- src/pages/assignment/AssignmentProjectData.jsx
+++ src/pages/assignment/AssignmentProjectData.jsx
@@ -1,4 +1,5 @@
 import project1 from "../../images/sipto.png";
+import project2 from "../../images/fit_ness.png";
 import hippatext from "../../images/hippatext.png";
 import calculator from "../../images/calculator.png";
 
@@ -8,6 +9,12 @@ const AssignmentProData = [
         title: "Sipto web-3.0",
         link: "https://manikantmani.github.io/sipto/",
         detail: "This website is besed on web-3.0 Fully decentralized. completely secure",
+    },
+    {
+        imgsrc: project2,
+        title: "Fit_ness",
+        link: "https://manikantmani.github.io/fit_ness/",
+        detail: "This is a fitness website. This website is fully responsive for mobilel and lablets.",
     },
     {
         imgsrc: hippatext,
